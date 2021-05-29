bde1d0cdb0918a0d80b82a14839379e2439e3628
camigm1 <camigm1@gmail.com>
2021-05-29 16:37:30 -0600

    more changes

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,13 @@
+
+
+const windchillValue = document.querySelector('#windchill');
+const averageTemp = document.querySelector('#averageTemp').innerHTML;
+const windSpeed = document.querySelector('#windSpeed').innerHTML;
+
+function windChill(averageTemp, windSpeed){
+    let windchill = 35.74 + 0.6215 *averageTemp - 35.75*windSpeed**0.16 + 0.4275*windSpeed**0.16;
+    if ( (windchill <= ('10 C') || windchill <= '50 F') &&  windspeed > '4.8 kilometers')
+    return windchill;
+ }
+
+ windchillValue.innerHTML= windChill(averageTemp, windSpeed);
