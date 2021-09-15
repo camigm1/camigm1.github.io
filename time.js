84dd3ee6032fdf55030e66a9cbb85bc11cdd948c
camigm1 <camigm1@gmail.com>
2021-09-14 19:11:33 -0600

    changes

---
+++ time.js
@@ -0,0 +1,4 @@
+if (Date.parse(document.lastModified) != 0)
+        document.write('<p><hr><small><i>Last modified: '
+                       + document.lastModified
+                       + '</i></small>');
