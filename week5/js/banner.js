092a783f885028186638fff09fdba2156cdcdd45
camigm1 <camigm1@gmail.com>
2021-10-14 19:02:21 -0600

    changed

---
+++ week5/js/banner.js
@@ -0,0 +1,19 @@
+let banner = document.querySelector(".banner");
+
+let daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = new Date();
+
+if (day == "Friday") {
+  return ``;
+}
+
+let banner = document.querySelector(".banner");
