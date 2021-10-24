c8c5ac7e4611779125476832eac2501113d21277
camigm1 <camigm1@gmail.com>
2021-10-23 21:40:39 -0600

    changes

--- week6/js/banner.js
+++ week6/js/banner.js
@@ -1,17 +1,14 @@
-let banner = document.querySelector(".banner")
+let banner = document.querySelector(".banner");
 
 let d = new Date();
 let currentDay = d.getDay();
 
-
-function displayBanner(){
-  if( currentDay === 6)
-  banner.style.display = block;
-} 
-  else {
-  banner.style.display = 'none';
-
-}
+function displayBanner() {
+  if (currentDay === 6) {
+    banner.style.display = block;
+  } else {
+    banner.style.display = "none";
+  }
 }
 
 displayBanner();
