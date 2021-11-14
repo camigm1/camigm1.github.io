ed1c7a4c500082493697077ca7d5e31c49b75cef
camigm1 <camigm1@gmail.com>
2021-11-13 19:47:19 -0700

    updates

--- week9/js/homepage.js
+++ week9/js/homepage.js
@@ -1,23 +1,23 @@
-const requestURL =
-  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    const prophets = jsonObject["prophets"];
-    console.log(prophets);
-    for (let i = 0; i < prophets.length; i++) {
+    const towns2 = jsonObject["towns"];
+
+    // console.log(towns2[(0, 6)]);
+    for (let i = 0; i < towns2.length; i++) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let image = document.createElement("img");
       let paragraph = document.createElement("p");
       let paragraphTwo = document.createElement("p");
-      h2.textContent = prophets[i].name + " " + prophets[i].lastname;
-      paragraph.innerHTML = `Date of Birth: ${prophets[i].birthdate}`;
-      paragraphTwo.innerHTML = `Birthplace: ${prophets[i].birthplace}`;
-      image.setAttribute("src", prophets[i].imageurl);
+      h2.textContent = `${towns2[i].name}`;
+      paragraph.innerHTML = `Year Founded: ${towns2[i].yearFounded}`;
+      paragraphTwo.innerHTML = `Population: ${towns2[i].currentPopulation}`;
+      image.setAttribute("src", towns2[i].imageurl);
       card.appendChild(h2);
       card.appendChild(paragraph);
       card.appendChild(paragraphTwo);
