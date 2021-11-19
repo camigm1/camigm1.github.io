d572768b495c2249a314375d463c39644f55a840
camigm1 <camigm1@gmail.com>
2021-11-18 18:59:28 -0700

    updates

--- week10/js/homepage.js
+++ week10/js/homepage.js
@@ -8,7 +8,16 @@ fetch(requestURL)
     const towns2 = jsonObject["towns"];
 
     // console.log(towns2[(0, 6)]);
+
     for (let i = 0; i < towns2.length; i++) {
+      if (
+        towns2[i] === towns2[1] ||
+        towns2[i] === towns2[3] ||
+        towns2[i] === towns2[4] ||
+        towns2[i] === towns2[5]
+      )
+        continue;
+
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let image = document.createElement("img");
@@ -17,7 +26,7 @@ fetch(requestURL)
       h2.textContent = `${towns2[i].name}`;
       paragraph.innerHTML = `Year Founded: ${towns2[i].yearFounded}`;
       paragraphTwo.innerHTML = `Population: ${towns2[i].currentPopulation}`;
-      image.setAttribute("src", towns2[i].photo);
+      image.setAttribute("src", `images/${towns2[i].photo}`);
       card.appendChild(h2);
       card.appendChild(paragraph);
       card.appendChild(paragraphTwo);
