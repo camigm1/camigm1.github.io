cf9b8db32817b36f153f267a97b7261dc3052822
camigm1 <camigm1@gmail.com>
2021-11-22 18:37:42 -0700

    updated

--- week10/js/script.js
+++ week10/js/script.js
@@ -14,3 +14,15 @@ window.onresize = () => {
     mainnav.classList.remove("responsive");
   }
 };
+
+//Form
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+  const s = document.querySelector("#selected");
+  const sel = document.querySelector("#selectbrowser");
+  s.style.display = "block";
+  s.textContent = sel.value;
+}
