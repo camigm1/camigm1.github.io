a8fd84d15f4b06ffbc082f2071e76609e58e634f
camigm1 <camigm1@gmail.com>
2021-11-28 12:20:51 -0700

    updated

--- week11/js/daysbetween.js
+++ week11/js/daysbetween.js
@@ -1,2 +1,62 @@
-const milliseconds = 8640000;
-const lastVisit = localStorage.getItem("lastvisit");
+/*let statement = document.querySelector(".last-visit");
+let localStorage = window.localStorage;
+let now = localStorage.getItem("newVisit");
+let previousVisit = localStorage.getItem("previousVisit");
+let toDisplay;
+let dayDiff;
+
+if (previousVisit == null) {
+  // Logic if its the first time visiting
+  localStorage.setItem("previousVisit", new Date());
+  toDisplay = "Congrats on visiting our site for the first time!";
+  document.getElementsByClassName(".last-visit").innerHTML = toDisplay;
+} else {
+  //logic if visiting again, in days
+  localStorage.setItem("newVisit", new Date());
+  let previousVisit = Date.parse(localStorage.previousVisit);
+  let singleDay = 86400000;
+  dayDiff =
+    Date.parse(localStorage.newVisit) - Date.parse(localStorage.previousVisit);
+  toDisplayMath = Math.round(dayDiff / singleDay);
+  toDisplay =
+    "It has been " + toDisplayMath + " days since you last visited this site.";
+
+  //Clear localStorage with below command
+  //localStorage.clear('newVisit');
+}*/
+
+var counterContainer = document.querySelector(".last-visit");
+let localStorage = window.localStorage;
+let now = localStorage.getItem("newVisit");
+let previousVisit = localStorage.getItem("previousVisit");
+let toDisplay;
+let dayDiff;
+
+if (previousVisit == null) {
+  // Logic if its the first time visiting
+  localStorage.setItem("previousVisit", new Date());
+  // toDisplay = "Congrats on visiting our site for the first time!";
+  document.getElementsByClassName(
+    ".last-visit"
+  ).innerHTML = `Last visit: ${previousVisit}`;
+} else {
+  //logic if visiting again, in days
+  localStorage.setItem("newVisit", new Date());
+  let previousVisit = Date.parse(localStorage.previousVisit);
+  let singleDay = 86400000;
+  dayDiff =
+    Date.parse(localStorage.newVisit) - Date.parse(localStorage.previousVisit);
+  toDisplayMath = Math.round(dayDiff / singleDay);
+  toDisplay =
+    "It has been " + toDisplayMath + " days since you last visited this site.";
+
+  //Clear localStorage with below command
+  //localStorage.clear('newVisit');
+}
+
+var counterContainer = document.querySelector(".last-visit");
+var visitCount = toDisplay;
+counterContainer.innerHTML = visitCount;
+
+console.log(previousVisit);
+console.log(toDisplay);
