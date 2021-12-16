c31ba5cc9abc85979598be45fcbd5f9a2634ed7b
camigm1 <camigm1@gmail.com>
2021-12-15 17:25:25 -0700

    update

--- lehichamberofcommerce/js/date.js
+++ lehichamberofcommerce/js/date.js
@@ -1,31 +1,38 @@
-let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  
-  let d = new Date();
-  let dayName = daynames[d.getDay()];
-  let monthName = months[d.getMonth()];
-  let year = d.getFullYear();
-  let fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-  
-  document.querySelector("#copyrightYear").textContent = fulldate;
+// let daynames = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// let months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// let d = new Date(document.lastModified);
+// let dayName = daynames[d.getDay()];
+// let monthName = months[d.getMonth()];
+// let year = d.getFullYear();
+// let fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
+// document.querySelector("#copyrightYear").textContent = fulldate;
+
+function lastDate() {
+  const latestDate = new Date(document.lastModified);
+  document.querySelector("#copyrightYear").innerHTML = latestDate;
+}
+
+lastDate();
